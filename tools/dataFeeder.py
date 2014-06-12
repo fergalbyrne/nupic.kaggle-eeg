9005480b83611299bdb6146c9aa006e629142acc
surge <sbryukov@gmail.com>
2014-06-12 15:09:26 +0400

    single csv for Patient_n

--- tools/dataFeeder.py
+++ tools/dataFeeder.py
@@ -1,36 +1,108 @@
 #!/usr/bin/env python
 
-import tarfile, os
+import tarfile, os, glob, csv
 from optparse import OptionParser
-import dataToCsv
 
-patient_name = "Patient_8"
-data_work_dir = "data"
-clips_label = "ictal"
+
+import numpy as np
+from scipy import io
+from optparse import OptionParser
+import  datetime,time
+
+current_time = datetime.datetime.now()
 
 class dataFeeder:
+	
 	tar_file = str()
 	tar = tarfile
+	patient_name = str()
+	clips_label = str()
+	data_work_dir = 'data'
+
+	#csv header for OPF
+	h_chanels = ['timestamp']
+	h_field_t = ['datatime']
+	h_delimer = ['T']
+
+	outputWriter = csv.writer
+	outputFile = file
 
 	def __init__(self,tar_file, patient, lable):
 		self.tar_file = tar_file
 		self.tar = tarfile.open(tar_file)
-		patient_name = patient
-		clips_label = lable
+		self.patient_name = patient
+		self.clips_label = lable
 
-	def next(self):
-		next_item = tarfile.TarInfo
-		for tarinfo in self.tar:
-			next_item = self.tar.next()
-			if any(patient_name in s for s in  next_item.name.split('/') ) and any( clips_label in s for s in  next_item.name.split('/') ) :
-				break
+		if not os.path.exists(self.data_work_dir):
+			os.makedirs(self.data_work_dir)
 
-		next_item.name = os.path.basename(next_item.name)
-		self.tar.extract(next_item,path=data_work_dir)
-		dataToCsv.convertNumpyToCsv(data_work_dir+'/'+next_item.name, data_work_dir+'/'+next_item.name+'.csv')
-		#os.remove(data_work_dir+'/'+next_item.name) 
+	def initCSV(self, ref_file_name):
+
+		print ref_file_name
+		memb=self.tar.getmember(ref_file_name)
+		memb.name = os.path.basename(ref_file_name)
+		self.tar.extract(memb,path=self.data_work_dir)
+
+		data = io.loadmat(self.data_work_dir+'/'+memb.name)
+  		np.set_printoptions(threshold=np.nan)
+
+  		print "freq: " + str(data['freq'])
+  		
+  		parNum = 0 
+  		for l_channel in data['channels'][0][0]:
+  			self.h_chanels.append('P'+str(parNum))
+  			self.h_field_t.append('float')
+  			self.h_delimer.append ('')
+  			parNum +=1
+  		
+  		self.outputFile = open(self.data_work_dir+'/'+self.patient_name+'_'+self.clips_label+'.csv', "w")
+  		self.outputWriter = csv.writer(self.outputFile)
+
+  		self.outputWriter.writerow(self.h_chanels)
+  		self.outputWriter.writerow(self.h_field_t)
+  		self.outputWriter.writerow(self.h_delimer)
+
+  		#os.remove(self.data_work_dir+'/'+memb.name)
+
+
+
+	def appendToCSV(self,mat_file_name):
+
+		memb=self.tar.getmember(mat_file_name)
+		memb.name = os.path.basename(mat_file_name)
+		self.tar.extract(memb,path=self.data_work_dir)
+
+		data = io.loadmat(self.data_work_dir+'/'+memb.name)
+  		np.set_printoptions(threshold=np.nan)
+
+  		for l_row in np.transpose(data['data']):    
+		    ttime = current_time + datetime.timedelta(microseconds=float(0.0001) )
+		    res = [ttime.strftime("%Y-%m-%d %H:%M:%S.%f").strip()] + list(l_row)
+		    self.outputWriter.writerow(res)
+
+
+  		os.remove(self.data_work_dir+'/'+memb.name)
+
+
+	def run(self):
+
+		fn_list = []
+		tik_num = []
+		for item in self.tar.getnames():
+			if any(self.patient_name in s for s in  item.split('/') ) and any( self.clips_label in s for s in  item.split('/') ) :
+				fn_list.append(item)
+				tik_num.append( int( item.split('/')[-1].split('_')[-1].split('.')[0])-1 )
+				
+		tik_num = sorted(range(len(tik_num)), key=lambda k: tik_num[k])
 		
-		return data_work_dir+'/'+next_item.name+'.csv'
+		self.initCSV(fn_list[tik_num[0]])
+
+		for i in tik_num:
+			print fn_list[i].split('/')[-1]
+			self.appendToCSV(fn_list[tik_num[i]])
+
+		print 'done!'
+		 	
 
 
 if __name__ == "__main__":
@@ -38,13 +110,15 @@ if __name__ == "__main__":
 
   (options, args) = parser.parse_args()
 
+  print args
+
   if len(args) != 3:
     parser.print_help()
     print
     raise(Exception("dataFeader.py clip.tar.gz Patient_8 ictal"))
 
   df = dataFeeder(args[0], args[1], args[2])
-  print df.next()
+  df.run()
   
   
 
