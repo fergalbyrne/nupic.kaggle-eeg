9ad7404ec8284067ffa783e75df2590e08e2689c
surge <sbryukov@gmail.com>
2014-06-15 17:55:38 +0400

    some bugs fixes

--- tools/dataFeeder.py
+++ tools/dataFeeder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import tarfile, os, glob, csv
+import tarfile, os, glob, csv, sys
 from optparse import OptionParser
 
 
@@ -18,6 +18,7 @@ class dataFeeder:
 	patient_name = str()
 	clips_label = str()
 	data_work_dir = 'data'
+	flag1 = False
 
 	#csv header for OPF
 	h_chanels = ['timestamp']
@@ -36,14 +37,54 @@ class dataFeeder:
 		if not os.path.exists(self.data_work_dir):
 			os.makedirs(self.data_work_dir)
 
+	def min_max(self):
+		readName = self.data_work_dir+'/'+self.patient_name+'_'+self.clips_label+'.csv'
+		writeName = self.data_work_dir+'/'+self.patient_name+'_'+self.clips_label+'_min_max.csv'
+		print 'Min\Max for ',readName
+
+		with open(writeName, 'w') as inf:
+			infWriter = csv.writer(inf)
+			with open(readName, 'r') as srcCsv:
+				outputReader = csv.reader(srcCsv)
+				column = 1                # the second column (Python counts from 0, per @MRAB's comment)
+				datatype = float          # or int, as appropriate (per MvG)
+				
+				col = outputReader.next()
+				outputReader.next()
+				outputReader.next()
+
+				min_array = []
+				max_array = []
+
+				for i in range( 1, len(col) ):
+					min_array.append(sys.maxint)
+					max_array.append(-sys.maxint)
+
+				for row in outputReader:   
+					
+					for i in range( 1, len(row) ):
+						
+						val = float(row[i]) 
+						
+						if val < min_array[i-1]:
+							min_array[i-1] = val
+
+				        if val > max_array[i-1]:
+				            max_array[i-1] = val
+				
+				print 'Max:', max_array	
+				print 'Min:', min_array	
+				
+	  			infWriter.writerow(col[1:])
+	  			infWriter.writerow(max_array)
+	  			infWriter.writerow(min_array)
+
+
+
+
 	def initCSV(self, ref_file_name):
 
-		print ref_file_name
-		memb=self.tar.getmember(ref_file_name)
-		memb.name = os.path.basename(ref_file_name)
-		self.tar.extract(memb,path=self.data_work_dir)
-
-		data = io.loadmat(self.data_work_dir+'/'+memb.name)
+		data = io.loadmat(ref_file_name)
   		np.set_printoptions(threshold=np.nan)
 
   		print "freq: " + str(data['freq'])
@@ -62,15 +103,20 @@ class dataFeeder:
   		self.outputWriter.writerow(self.h_field_t)
   		self.outputWriter.writerow(self.h_delimer)
 
-  		#os.remove(self.data_work_dir+'/'+memb.name)
-
 
 
 	def appendToCSV(self,mat_file_name):
+		print mat_file_name
 
 		memb=self.tar.getmember(mat_file_name)
 		memb.name = os.path.basename(mat_file_name)
 		self.tar.extract(memb,path=self.data_work_dir)
+
+		if self.flag1 is False:
+			self.flag1 = True
+			self.initCSV(self.data_work_dir+'/'+memb.name)
+			print self.flag1
+
 
 		data = io.loadmat(self.data_work_dir+'/'+memb.name)
   		np.set_printoptions(threshold=np.nan)
@@ -89,18 +135,19 @@ class dataFeeder:
 		fn_list = []
 		tik_num = []
 		for item in self.tar.getnames():
-			if any(self.patient_name in s for s in  item.split('/') ) and any( self.clips_label in s for s in  item.split('/') ) :
-				fn_list.append(item)
-				tik_num.append( int( item.split('/')[-1].split('_')[-1].split('.')[0])-1 )
-				
+			
+			if any(self.patient_name in s for s in  item.split('/') ):
+				if self.clips_label in item.split('/')[-1].split('_'):
+					fn_list.append(item)
+					tik_num.append( int( item.split('/')[-1].split('_')[-1].split('.')[0])-1 )
+					
 		tik_num = sorted(range(len(tik_num)), key=lambda k: tik_num[k])
 		
-		self.initCSV(fn_list[tik_num[0]])
-
 		for i in tik_num:
-			print fn_list[i].split('/')[-1]
-			self.appendToCSV(fn_list[tik_num[i]])
+			self.appendToCSV(fn_list[i])
 
+		
+		#self.min_max()
 		print 'done!'
 		 	
 
@@ -119,6 +166,7 @@ if __name__ == "__main__":
 
   df = dataFeeder(args[0], args[1], args[2])
   df.run()
+  #df.min_max()
   
   
 
